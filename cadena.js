c0606d6d6dc8d4e83321651ec1ee4907612880ee
Javi <javicb55@gmail.com>
2017-10-05 11:09:13 +0200

    commit con archivos js

---
+++ cadena.js
@@ -0,0 +1,34 @@
+/*for (var i = 2; i < process.argv.length ; i++){
+	console.log('Argumento ' + i + ': ' + process.argv[i]);
+}*/
+
+if (process.argv.length < 3){
+	process.exit(1);
+}
+
+console.log('Cadena leida: ' + process.argv[2]);
+var cadena = process.argv[2];
+
+function procesarCadena(valorCadena){
+	var _cadena = valorCadena;
+	//Todo lo que define aquí, es privado
+	return {
+		longitudCadena    : function() { return _cadena.length; },
+		cadenaEsNumero    : function() { return !isNaN(cadena); },
+		cadenaEnMayuscula : function() { return _cadena.toUpperCase();},
+		cadenaAlReves	  : function() { return _cadena.split('').reverse().join('');},
+		esPalindromo	  : function() { return _cadena === this.cadenaAlReves();} 					
+	};
+}
+
+
+var cadena1 = procesarCadena(cadena);
+
+console.log("Longitud de la cadena: " + cadena + " es: " +
+cadena1.longitudCadena());
+console.log("La cadena " + cadena + " ¿es un número? " + cadena1.cadenaEsNumero());
+console.log("La cadena " + cadena + " en mayúsculas " + cadena1.cadenaEnMayuscula());
+console.log("La cadena " + cadena + " separada " + cadena1.cadenaAlReves());
+console.log("La cadena " + cadena + " ¿es palindromo? " + cadena1.esPalindromo());
+
+
---
+++ pasaporte.js
@@ -0,0 +1,14 @@
+if (process.argv.length < 3){
+	process.exit(1);
+}
+
+var pasaporte = process.argv[2];
+
+function identificarPasaporte(valorPasaporte){
+	var _pasaporte = valorPasaporte;
+	return{
+	
+	
+	}
+
+}
