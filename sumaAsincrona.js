4766f7c277f14478e5cdc4880a20c8547435bc27
Javi <javicb55@gmail.com>
2017-10-19 12:44:49 +0200

    Commits de suma sincronas y asincronas

---
+++ sumaAsincrona.js
@@ -0,0 +1,12 @@
+function suma (sumando1, sumando2, callback){
+  setTimeout(function (){
+    var resultado = sumando1 + sumando2;
+    callback(resultado);
+  }, 500);
+}
+
+var resultado = suma(2,3, function(datos){
+  console.log(datos);
+});
+
+console.log('fin de programa');
